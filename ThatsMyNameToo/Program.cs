98738d059b7bcc3cb30847e164471504e1cd628a
JustinBrown123 <j-brown123@live.com>
2019-09-05 12:49:33 -0400

    first

---
+++ ThatsMyNameToo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ThatsMyNameToo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats your name");
+            string name = Console.ReadLine().ToLower();
+
+            bool isJen = name.Equals("jen");
+            bool isSarah = name.Equals("sarah");
+
+            switch (name)
+            {
+                case "jen":
+                    Console.WriteLine("that's mine");
+                    break;
+
+                case "sarah":
+                    Console.WriteLine("thats my daughters name");
+                    break;
+
+                default:
+                    Console.WriteLine("Hi");
+                    break;
+            }
+            }
+        }
+    }
+
